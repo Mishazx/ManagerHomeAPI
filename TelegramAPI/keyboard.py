26062ede231244deb5530a8f5352f44a192d27f9
Mishazx <michael.konowalov@mail.ru>
2023-12-30 09:48:22 +0300

    add settings keyboard tg bot

--- TelegramAPI/keyboard.py
+++ TelegramAPI/keyboard.py
@@ -1,4 +1,7 @@
 from telebot import types
+from django.contrib.auth.models import User
+
+from YandexAPI.models import Device, OAuthKey
 
 
 def create_MainKeyboard():
@@ -9,4 +12,27 @@ def create_MainKeyboard():
 
     keyboard.add(button1, button2)
     
+    return keyboard
+
+def create_SettingsKeyboard(username):
+
+    user = User.objects.get(username=username)
+    oauth_key = OAuthKey.objects.filter(user=user).exists()
+    
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True)
+
+    if oauth_key:
+        buttonUnlink = types.KeyboardButton("Отвязать аккаунт 'Яндекс'")
+        buttonRegisterDevices = types.KeyboardButton("Перерегистрировать все устройства")
+
+        keyboard.add(buttonUnlink, buttonRegisterDevices)
+        
+    else:
+        buttonPri = types.KeyboardButton("Привязать аккаунт 'Яндекс'")
+        keyboard.add(buttonPri)
+
+    buttonMain = types.KeyboardButton("Главное меню")
+    buttonTest = types.KeyboardButton("Новые функции")
+    keyboard.add(buttonMain, buttonTest)
+
     return keyboard
